e90e2b95cae73b0ee55c20b5423c39b34036dc5a
DavyBee <david.beeson123@gmail.com>
2022-12-13 22:57:57 -0500

    Adding Statistics to the Project hopefully

---
+++ Statistics For Project/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.statisticsforproject {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.example.statisticsforproject to javafx.fxml;
+    exports com.example.statisticsforproject;
+    exports;
+    opens to
+}
